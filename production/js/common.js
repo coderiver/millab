e415e2e5a325e493d2c4568e92aa2ae9564306b7
Eugene Godun <eugene.godun@gmail.com>
2014-09-23 16:26:46 +0300

    index

--- production/js/common.js
+++ production/js/common.js
@@ -4,5 +4,29 @@ head.ready(function() {
 	// 	$(".js-popup").hide();
 	// });
 
-	console.log($('body').html());
+	$('.js-slick').slick({
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		dots: false,
+		arrows: true
+	});
+	var win = $(".js-window");
+	$(".js-window-toggle").hover(
+	  function() {
+	  	var href = $(this).attr("href");
+	  	var el = $('[data-window="'+href+'"]');
+	  	var el_left = $(this).offset().left;
+	  	var el_top = $(this).offset().top;
+		win.hide();
+		el.css({
+			left: el_left - $(this).outerWidth()/2,
+			top: el_top - el.outerHeight()
+		});
+	    el.fadeIn(200);
+	  }, function() {
+
+	    win.hide();
+	  }
+	);
 });
