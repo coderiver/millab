144a698fc9e4e927f3262abf0674ef9e5f6928f1
whats0n <mrdvv@mail.ru>
2014-11-18 11:26:34 +0200

    print

--- production/js/init.js
+++ production/js/init.js
@@ -1,6 +1,6 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/slick.min.js",
-					"js/lib/cycle.min.js",
+					"js/lib/cycle.js",
 					"js/lib/fancybox.js",
 					"js/lib/jquery.validate.js",
 					"js/lib/jquery.maskedinput.js",
